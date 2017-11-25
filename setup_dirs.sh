606394bb70eb151c9f1a34169605815751d7637b
Lukas Prantl <lukas_prantl@hotmail.de>
2017-11-25 09:46:15 +0100

    moved model dir

--- setup_dirs.sh
+++ setup_dirs.sh
@@ -3,6 +3,7 @@ mkdir -p 2D_data/
 mkdir -p 2D_data/lowres
 mkdir -p 2D_data/highres
 mkdir -p 2D_data/log
+mkdir -p models/2D_data/
 mkdir -p 2D_data/patches
 mkdir -p 2D_data/patches/lowres
 mkdir -p 2D_data/patches/highres
