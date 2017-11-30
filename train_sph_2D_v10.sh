04ae6d178547afce85f99f89bc083e8b954a08eb
Lukas Prantl <lukas_prantl@hotmail.de>
2017-11-30 02:12:48 +0100

    updated v10 model

--- train_sph_2D_v10.sh
+++ train_sph_2D_v10.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-python3 2D_SPH/scenes/tools/train_sequential.py ref 2D_data/patches/highres/ref_sph_2D_v02-05_d%03d_var%02d_%03d time_end 15 features ps,dens,vel data_end 18 fig models/2D/sph_2D_v10_loss var 1 checkpoint_intervall 10 model models/2D/sph_2D_v10 val_split 0.2 src 2D_data/patches/lowres/sph_2D_v02-05_d%03d_var%02d_%03d log_intervall 10 epochs 250 time_start 5 batch 32 data_start 0
+python3 2D_SPH/scenes/tools/train_sequential.py var 1 epochs 250 data_start 0 val_split 0.2 time_start 5 ref 2D_data/patches/highres/ref_sph_2D_v02-01_d%03d_var%02d_%03d batch 32 data_end 18 log_intervall 10 fig models/2D/sph_2D_v10_loss model models/2D/sph_2D_v10 checkpoint_intervall 10 time_end 15 src 2D_data/patches/lowres/sph_2D_v02-01_d%03d_var%02d_%03d features ps,dens,vel
