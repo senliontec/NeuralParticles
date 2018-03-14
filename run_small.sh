d692dbed96f8178716c5ffe0c15aa8f0f2c2e74f
Lukas Prantl <lukas_prantl@hotmail.de>
2018-03-14 22:44:42 +0100

    pyhton3 to python

--- run_small.sh
+++ run_small.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-python3 gen_src.py config $1 data $2 manta $3
-python3 train.py config $1 data $2 manta $3
-python3 run.py config $1 data $2
-python3 show_data.py config $1 data $2 manta $3 type "res"
+python gen_src.py config $1 data $2 manta $3
+python train.py config $1 data $2 manta $3
+python run.py config $1 data $2
+python show_data.py config $1 data $2 manta $3 type "res"
