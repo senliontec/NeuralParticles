8f21e5d35be5b05afb3704c8741b1cd4b83fb0bd
Prantl <lukas_prantl@hotmail.de>
2018-03-17 21:57:31 +0100

    removed files

--- tmp_run.sh
+++
@@ -1,4 +0,0 @@
-python3 train.py config config/version_02.txt data 2D_data/
-python3 train.py config config/version_03.txt data 2D_data/
-python3 train.py config config/version_01.txt data 2D_data/
-python3 train.py config config/version_00.txt data 2D_data/
