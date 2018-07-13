a9f055de7acf38dabf31f5603b985c1b063cfaa4
Unknown <lukas_prantl@hotmail.de>
2018-07-13 16:57:04 +0200

    improved visualization of data

--- neuralparticles/scenes/show_particles.py
+++ neuralparticles/scenes/show_particles.py
@@ -5,10 +5,13 @@ from param_helpers import *
 guion = True
 
 in_path = getParam("in", "")
-sdf_path = getParam("sdf", "")
+src_path = getParam("src", "")
+ref_path = getParam("ref", "")
 
 res  = int(getParam("res", 150))
 sres = int(getParam("sres", 2))
+
+fac = float(getParam("fac", 1.))
 
 t = int(getParam("t", 50))
 t_start = int(getParam("t_start", 0))
@@ -25,10 +28,9 @@ checkUnusedParams()
 pause = screenshot == ""
 
 gs = vec3(res, res, 1 if dim == 2 else res)
-
 gs_show = vec3(res, res, 3 if dim == 2 else res)
 
-s = Solver(name='IISPH', gridSize=gs, dim=dim)
+s = Solver(name='high', gridSize=gs, dim=dim)
 s_show = Solver(name="show", gridSize=gs_show, dim=3)
 
 pp = s.create(BasicParticleSystem)
@@ -43,36 +45,69 @@ flags_show.initDomain()
 flags_show.fillGrid(TypeEmpty)
 
 gFlags   = s.create(FlagGrid)
-
 gFlags.initDomain(4)
 
 gIdxSys  = s.create(ParticleIndexSystem)
 gIdx     = s.create(IntGrid)
 gCnt     = s.create(IntGrid)
 
+if ref_path != "":
+	ref_pp = s.create(BasicParticleSystem)
+	ref_sdf = s.create(LevelsetGrid)
+	ref_mesh = s_show.create(Mesh)
+
+if src_path != "":
+	src_pp = s.create(BasicParticleSystem)
+	src_sdf = s.create(LevelsetGrid)
+	src_mesh = s_show.create(Mesh)
+	
 if guion:
 	gui = Gui()
 	gui.show()
 	if pause: gui.pause()
 
 for i in range(t_start,t_end):
-	if sdf_path != "":
+	'''if sdf_path != "":
 		sdf.load(sdf_path % i)
 		sdf.reinitMarching(flags=gFlags)
 		pp.clear()
 		sampleLevelsetWithParticles(phi=sdf, flags=gFlags, parts=pp, discretization=sres, randomness=0)
-	else:
-		pp.load(in_path % i)
-		gridParticleIndex(parts=pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, counter=gCnt)
-		unionParticleLevelset(parts=pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, phi=sdf, radiusFactor=2.0, exclude=FlagObstacle)
-		extrapolateLsSimple(phi=sdf, distance=4, inside=True)
-
+	else:'''
+	pp.load(in_path % i)
+	gridParticleIndex(parts=pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, counter=gCnt)
+	unionParticleLevelset(parts=pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, phi=sdf, radiusFactor=2.0, exclude=FlagObstacle)
+	extrapolateLsSimple(phi=sdf, distance=4, inside=True)
+	sdf.setBound(value=5., boundaryWidth=4)
 	if dim == 2:
 		placeGrid2d(sdf,sdf_show,dstz=1) 
 		sdf_show.createMesh(mesh)
 	else:
 		sdf.createMesh(mesh)
+
+	if ref_path != "":
+		ref_pp.load(ref_path % i)
+		gridParticleIndex(parts=ref_pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, counter=gCnt)
+		unionParticleLevelset(parts=ref_pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, phi=ref_sdf, radiusFactor=2.0, exclude=FlagObstacle)
+		extrapolateLsSimple(phi=ref_sdf, distance=4, inside=True)
+		ref_sdf.setBound(value=5., boundaryWidth=4)
+		if dim == 2:
+			placeGrid2d(ref_sdf,sdf_show,dstz=1) 
+			sdf_show.createMesh(ref_mesh)
+		else:
+			ref_sdf.createMesh(ref_mesh)
 	
+	if src_path != "":
+		src_pp.load(src_path % i)
+		gridParticleIndex(parts=src_pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, counter=gCnt)
+		unionParticleLevelset(parts=src_pp, indexSys=gIdxSys, flags=gFlags, index=gIdx, phi=src_sdf, radiusFactor=2.0, exclude=FlagObstacle)
+		extrapolateLsSimple(phi=src_sdf, distance=4, inside=True)
+		src_sdf.setBound(value=5., boundaryWidth=4)
+		if dim == 2:
+			placeGrid2d(src_sdf,sdf_show,dstz=1) 
+			sdf_show.createMesh(src_mesh)
+		else:
+			src_sdf.createMesh(src_mesh)
+
 	s.step()
 	if screenshot != "":
 		gui.screenshot(screenshot % i if t > 1 else screenshot)
--- neuralparticles/scripts/show_data.py
+++ neuralparticles/scripts/show_data.py
@@ -13,15 +13,13 @@ manta_path = getParam("manta", "neuralparticles/")
 config_path = getParam("config", "config/version_00.txt")
 verbose = int(getParam("verbose", 0)) != 0
 
-src = getParam("src", "")
-
 t_start = int(getParam("t_start", -1))
 t_end = int(getParam("t_end", -1))
 
 dataset = int(getParam("dataset", -1))
 var = int(getParam("var", 0))
 
-data_type = getParam("type", "ref")
+show_results = int(getParam("res", 1)) != 0
 
 scr = getParam("scr", "")
 
@@ -42,39 +40,21 @@ with open(os.path.dirname(config_path) + '/' + config['train'], 'r') as f:
 param = {}
 
 if dataset < 0:
-    dataset = int(data_config['data_count']*train_config['train_split']) if data_type == 'res' else 0
+    dataset = int(data_config['data_count']*train_config['train_split']) if show_results else 0
 
 dim = data_config['dim']
 res = data_config['res']
-if data_type == "ref":
-    data_path += "reference/%s_%s_d%03d" % (data_config['prefix'], data_config['id'], dataset)
-    param['in'] = data_path + "_%03d_ps.uni"
-    param['sdf'] = data_path + "_%03d_sdf.uni"
-elif data_type == "real":
-    data_path += "real/%s_%s_d%03d" % (data_config['prefix'], data_config['id'], dataset)
-    res = int(res/math.pow(pre_config['factor'], 1/dim))
-    param['in'] = data_path + "_%03d_ps.uni"
-    param['sdf'] = data_path + "_%03d_sdf.uni"
-elif data_type == "src":
-    data_path += "source/%s_%s-%s_d%03d_var%02d" % (data_config['prefix'], data_config['id'], pre_config['id'], dataset, var)
-    res = int(res/math.pow(pre_config['factor'], 1/dim))
-    param['in'] = data_path + "_%03d_ps.uni"
-    param['sdf'] = data_path + "_%03d_sdf.uni"
-elif data_type == "res":
-    data_path += "result/%s_%s-%s_d%03d_var%02d" % (data_config['prefix'], data_config['id'], pre_config['id'], dataset, var)
-    param['in' if train_config['explicit'] else 'sdf'] = data_path + "_result_%03d.uni"
-elif data_type == "res_real":
-    data_path += "result/%s_%s_d%03d" % (data_config['prefix'], data_config['id'], dataset)
-    param['in' if train_config['explicit'] else 'sdf'] = data_path + "_result_%03d.uni"
-else:
-    print("data type not supported!")
-    exit()
+param['in'] = data_path + "result/%s_%s-%s_d%03d_var%02d_result" % (data_config['prefix'], data_config['id'], pre_config['id'], dataset, var) + "_%03d.uni"
+param['src'] = data_path + "source/%s_%s-%s_d%03d_var%02d" % (data_config['prefix'], data_config['id'], pre_config['id'], dataset, var) + "_%03d_ps.uni"
+param['ref'] = data_path + "reference/%s_%s_d%03d" % (data_config['prefix'], data_config['id'], dataset) + "_%03d_ps.uni"
+
+if not show_results:
+    param['in'] = param.pop('ref')
 
 if t_start < 0:
     t_start = min(train_config['t_start'], data_config['frame_count']-1)
 if t_end < 0:
     t_end = min(train_config['t_end'], data_config['frame_count'])
-
 
 param['t_start'] = t_start
 param['t_end'] = t_end
