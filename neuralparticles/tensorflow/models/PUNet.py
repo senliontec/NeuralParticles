0f3e22284dfd307930216d0220702abe03622de1
Unknown <lukas_prantl@hotmail.de>
2018-11-05 10:10:40 +0100

    changed load function of punet

--- neuralparticles/tensorflow/models/PUNet.py
+++ neuralparticles/tensorflow/models/PUNet.py
@@ -322,9 +322,13 @@ class PUNet(Network):
         self.model.save(path)
 
     def load_model(self, path):
-        self.model = load_model(path, custom_objects={'mask_loss': self.mask_loss, 'trunc_loss': self.trunc_loss, 'temp_loss': self.temp_loss, 
-                                                      'particle_metric': self.particle_metric, 'trunc_metric': self.trunc_metric,
-                                                      'Interpolate': Interpolate, 'SampleAndGroup': SampleAndGroup, 'MultConst': MultConst, 'AddConst': AddConst})
+        if self.model:
+            self.model.load_weights(path)
+            self.train_model.load_weights(path)
+        else:
+            self.model = load_model(path, custom_objects={'mask_loss': self.mask_loss, 'trunc_loss': self.trunc_loss, 'temp_loss': self.temp_loss, 
+                                                          'particle_metric': self.particle_metric, 'trunc_metric': self.trunc_metric,
+                                                          'Interpolate': Interpolate, 'SampleAndGroup': SampleAndGroup, 'MultConst': MultConst, 'AddConst': AddConst})
         '''if self.truncate:
             out, trunc = self.model.outputs
             trunc_exp = stack([trunc, trunc, trunc], 2)
